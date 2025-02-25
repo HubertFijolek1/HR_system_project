eede3c3f9a517fa51099920ed840f389a77b7e42
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-02-25 14:43:44 +0100

    "feat: Implement candidate registration logic using DRF"

--- recruitment/serializers.py
+++ recruitment/serializers.py
@@ -5,3 +5,6 @@ class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
         fields = '__all__'
+    
+    def validate_email(self, value):
+        return value.lower()
