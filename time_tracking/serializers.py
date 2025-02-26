82f9dd44d75cad84f749896e1ac7f3d30723169a
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-02-26 10:56:50 +0100

    fix: Correct datetime format in TimeEntrySerializer

--- time_tracking/serializers.py
+++ time_tracking/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import TimeEntry
 
 class TimeEntrySerializer(serializers.ModelSerializer):
+    clock_in = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S")
+    clock_out = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", required=False, allow_null=True)
+    
     class Meta:
         model = TimeEntry
-        fields = '__all__'
+        fields = "__all__"
