74a360f5f69660d6e12c2dcbee796340ca077ffa
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-02-26 10:53:49 +0100

    feat: Implement recruitment reports endpoint

--- recruitment/views.py
+++ recruitment/views.py
@@ -3,5 +3,18 @@ from .models import Candidate
 from .serializers import CandidateSerializer
 
 class CandidateViewSet(viewsets.ModelViewSet):
+    
+    from rest_framework.decorators import action
+    from rest_framework.response import Response
+    
+    @action(detail=False, methods=["get"], url_path="reports")
+    def reports(self, request):
+        total_candidates = self.get_queryset().count()
+        report = {
+            "total_candidates": total_candidates,
+            # Further report calculations can be added here
+        }
+        return Response(report)
+
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
