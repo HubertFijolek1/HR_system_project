3ba34d53dac6c99e1a1fa9f120255abf42a085ae
HubertFijolek1 <hubertfijolek1@gmail.com>
2025-02-26 10:37:53 +0100

    feat: Implement API for time tracking entries

--- hr_system/urls.py
+++ hr_system/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/recruitment/', include('recruitment.urls')),
+    path('api/time_tracking/', include('time_tracking.urls'))
 ]
--- time_tracking/views.py
+++ time_tracking/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TimeEntry
+from .serializers import TimeEntrySerializer
 
-# Create your views here.
+class TimeEntryViewSet(viewsets.ModelViewSet):
+    queryset = TimeEntry.objects.all()
+    serializer_class = TimeEntrySerializer
+EOF
